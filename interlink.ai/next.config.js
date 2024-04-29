e2d1ae0684439931d5f35ba182c323d7a6eefa2d
vlk-louis <77217964+vlk-louis@users.noreply.github.com>
2024-04-29 17:20:35 +0700

    config2

--- interlink.ai/next.config.js
+++ interlink.ai/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
   output: 'export',
+  images: {
+    unoptimized: true  // Disables the built-in Image Optimization API
+  }
 };
 
 module.exports = nextConfig;
